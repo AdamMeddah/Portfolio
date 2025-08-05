ea264179785767de6af158c9af6d49240407206b
AdamMeddah <78007082+AdamMeddah@users.noreply.github.com>
2025-08-05 01:46:37 -0400

    Main preview div & Unit changes
    Working on learning about position absolute, relative, etc. Using rem and top, left, %, etc.
    
    Added main video preview & two svgs. (Info icon & Play icon)

--- src/components/tabs/MainScreen.jsx
+++ src/components/tabs/MainScreen.jsx
@@ -24,6 +24,39 @@ export default function MainScreen({ user }) {
           <img className="user-logo" src={getUserLogo(user)} alt="" />
         </div>
       </nav>
+
+      <div className="main-preview">
+        <video
+          className="main-preview-vid"
+          autoPlay
+          loop
+          src="videos/noted.mp4"
+        ></video>
+
+        <svg
+          className="info-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#000000"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <circle cx="12" cy="12" r="10"></circle>
+          <line x1="12" y1="16" x2="12" y2="12"></line>
+          <line x1="12" y1="8" x2="12.01" y2="8"></line>
+        </svg>
+
+        <svg className="play-icon" viewBox="0 0 30 30 ">
+          <path
+            d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z"
+            fill="black"
+          />
+        </svg>
+      </div>
     </>
   );
 }
