1e4f9265284607c22251aef6e5dbd2b4658ee851
AdamMeddah <78007082+AdamMeddah@users.noreply.github.com>
2025-08-22 01:07:44 -0400

    beginning tag system (& Tag Component)

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,24 @@
+function Tag({ img, title }) {
+  return (
+    <div className="tag">
+      <img className="tag-image" src={img} alt={title} />
+      <h3>{title}</h3>
+    </div>
+  );
+}
+
 export default function Project({ title, desc, image }) {
   return (
     <div className="project">
-      <img className="project-image" src={image} alt="" />
+      <img className="project-image" src={image} alt={title + "logo"} />
       <div className="project-text">
         <h1 className="project-title">{title}</h1>
 
         <h2 className="project-desc">{desc}</h2>
+      </div>
+
+      <div className="tags">
+        <Tag img="vite.svg" title="Vite" />
       </div>
     </div>
   );
